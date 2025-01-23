71a29739e11f250078411539e16b879921fb0c62
이승진 <lsj952010@gmail.com>
2025-01-24 08:20:21 +0900

    [STEP12] 동시성 제어 방식 적용 및 테스트 코드 작성 - 예약

---
+++ concert-reservation/src/main/java/com/concert/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.concert.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.redisson.config.Config;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + host + ":" + port);
+        return Redisson.create(config);
+    }
+}
